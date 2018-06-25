a8bd66f6ae8a387ab78d5b2121631d1d20c57de0
Will JV Smith <smithwjv@gmail.com>
2018-06-25 14:05:05 +0100

    Keep the screen on while the app is open

--- app/src/main/java/smithwjv/rowingstrokemeter/MainActivity.java
+++ app/src/main/java/smithwjv/rowingstrokemeter/MainActivity.java
@@ -11,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.SeekBar;
@@ -54,6 +55,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         recordingProgressBar = (ProgressBar) findViewById(R.id.pb_recording);
 
